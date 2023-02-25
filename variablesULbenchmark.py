3468b804e0aaec01c6b378a8413def485dc423e0
Tommaso Tedeschi <tedesk95@gmail.com>
2023-02-25 19:14:07 +0100

    Update variablesULbenchmark.py

--- variablesULbenchmark.py
+++ variablesULbenchmark.py
@@ -31,21 +31,21 @@ class variable(object):
 variables = {}
 
 variables["SR"] = []
-variables["SR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 6 , array("f", [30., 45., 60., 80., 100., 150, 250.]) ))
+variables["SR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 7 , array("f", [0.,30., 45., 60., 80., 100., 150, 250.]) ))
 variables["SR"].append(variable('tau_pt',  'Tau p_{T} [GeV]', 3 , array("f", [30., 45., 60., 100.])))
 variables["SR"].append(variable('m_jj', 'Invarant mass JJ',  4, array("f", [500., 700., 1000., 1500., 2500.])))
 
 variables["fakes_CR"] = []
-variables["fakes_CR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 6 , array("f", [30., 45., 60., 80., 100., 150, 250.]) ))
+variables["fakes_CR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 7 , array("f", [0., 30., 45., 60., 80., 100., 150, 250.]) ))
 variables["fakes_CR"].append(variable('tau_pt',  'Tau p_{T} [GeV]', 3 , array("f", [30., 45., 60., 100.])))
 variables["fakes_CR"].append(variable('m_jj', 'Invarant mass JJ',  6, array("f",  [0., 300., 500., 700., 1000., 1500., 2000.])))
 
 variables["ttbar_CR"] = []
-variables["ttbar_CR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 8 , array("f", [30., 45., 60., 80., 100., 125., 150, 200., 250.]) ))
+variables["ttbar_CR"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 9 , array("f", [0., 30., 45., 60., 80., 100., 125., 150, 200., 250.]) ))
 variables["ttbar_CR"].append(variable('tau_pt',  'Tau p_{T} [GeV]', 8 , array("f", [30., 45., 60., 80., 100., 125., 150, 200., 250.])))
 variables["ttbar_CR"].append(variable('m_jj', 'Invarant mass JJ',  6, array("f", [0., 300., 500., 700., 1000., 1500., 2000.])))
 
 variables["OS_CR_bvetoL"] = []
-variables["OS_CR_bvetoL"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 8 , array("f",  [30., 45., 60., 80., 100., 125., 150, 200., 250.]) ))
+variables["OS_CR_bvetoL"].append(variable('lepton_pt',  'Lepton p_{T} [GeV]', 9 , array("f",  [0., 30., 45., 60., 80., 100., 125., 150, 200., 250.]) ))
 variables["OS_CR_bvetoL"].append(variable('tau_pt',  'Tau p_{T} [GeV]', 8 , array("f", [30., 45., 60., 80., 100., 125., 150, 200., 250.])))
 variables["OS_CR_bvetoL"].append(variable('m_jj', 'Invarant mass JJ',  6, array("f", [0., 300., 500., 700., 1000., 1500., 2000.])))
